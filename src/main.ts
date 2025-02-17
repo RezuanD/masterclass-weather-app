ec8ba8f5efff4975d768ff331365fc59dd6458ce
Rez Dzibov <dzibov.fortech@gmail.com>
2025-02-17 11:25:53 +0300

    feat: Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appPort = process.env.PORT ?? 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Weather API')
+    .setDescription('Weather API description')
+    .setVersion('0.0.1')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(appPort);
 
   const logger = new Logger();
